6c64c393b84a95d2dbb508560915b312155b19bd
Stephen Reynolds <sdreynolds110@gmail.com>
2021-10-08 15:04:50 -0400

    small edits

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="nav">
+                <h1 className="navbar-brand">Health<i className="fas fa-arrows-alt-h"></i>Span</h1>
+            </div>
+        );
+    }
+}
---
+++ src/Components/Provider.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Provider extends Component {
+    render() {
+        return (
+            <div className="provider">
+                <h1 className="provider-body">Body</h1>
+            </div>
+        );
+    }
+}
